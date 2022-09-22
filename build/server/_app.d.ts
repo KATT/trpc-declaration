f9d18190256e52e96c0613ba2b165914af83b268
KATT <alexander@n1s.se>
2022-09-22 21:54:37 +0200

    build

---
+++ build/server/_app.d.ts
@@ -0,0 +1,58 @@
+declare const appRouter: import("@trpc/server").Router<import("@trpc/server").RouterDef<{}, import("@trpc/server").DefaultErrorShape, {}, {
+    greeting: import("@trpc/server").QueryProcedure<import("@trpc/server/dist/core/internals/utils").OverwriteKnown<{
+        _config: {
+            ctx: {};
+            meta: {};
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        };
+        _meta: {};
+        _ctx_in: {};
+        _ctx_out: {};
+        _input_in: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _input_out: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        _output_in: {
+            text: string;
+        };
+        _output_out: {
+            text: string;
+        };
+    }>>;
+}>> & {
+    greeting: import("@trpc/server").QueryProcedure<import("@trpc/server/dist/core/internals/utils").OverwriteKnown<{
+        _config: {
+            ctx: {};
+            meta: {};
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        };
+        _meta: {};
+        _ctx_in: {};
+        _ctx_out: {};
+        _input_in: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _input_out: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        _output_in: {
+            text: string;
+        };
+        _output_out: {
+            text: string;
+        };
+    }>>;
+};
+export declare type AppRouter = typeof appRouter;
+export {};
+//# sourceMappingURL=_app.d.ts.map
---
+++ build/utils/trpc.d.ts
@@ -0,0 +1,56 @@
+export declare const trpc: import("@trpc/react/dist/createTRPCReact").CreateTRPCReact<import("@trpc/server").Router<import("@trpc/server").RouterDef<{}, import("@trpc/server").DefaultErrorShape, {}, {
+    greeting: import("@trpc/server").QueryProcedure<import("@trpc/server/dist/core/internals/utils").OverwriteKnown<{
+        _config: {
+            ctx: {};
+            meta: {};
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        };
+        _meta: {};
+        _ctx_in: {};
+        _ctx_out: {};
+        _input_in: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _input_out: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        _output_in: {
+            text: string;
+        };
+        _output_out: {
+            text: string;
+        };
+    }>>;
+}>> & {
+    greeting: import("@trpc/server").QueryProcedure<import("@trpc/server/dist/core/internals/utils").OverwriteKnown<{
+        _config: {
+            ctx: {};
+            meta: {};
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        };
+        _meta: {};
+        _ctx_in: {};
+        _ctx_out: {};
+        _input_in: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _input_out: {
+            name?: string | null | undefined;
+        } | null | undefined;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        _output_in: {
+            text: string;
+        };
+        _output_out: {
+            text: string;
+        };
+    }>>;
+}, unknown>;
+//# sourceMappingURL=trpc.d.ts.map
