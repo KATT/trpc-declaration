c90a606888788a00acc64a641968b4871cc85a81
KATT <alexander@n1s.se>
2022-09-22 21:51:29 +0200

    clean

--- src/server/_app.ts
+++ src/server/_app.ts
@@ -2,9 +2,8 @@
  * This is the API-handler of your app that contains all your API routes.
  * On a bigger app, you will probably want to split this file up into multiple files.
  */
-import { initTRPC } from '@trpc/server';
-import * as trpcNext from '@trpc/server/adapters/next';
-import { z } from 'zod';
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
 
 const t = initTRPC.create();
 
@@ -22,7 +21,7 @@ const appRouter = t.router({
     .query(({ input }) => {
       // This is what you're returning to your client
       return {
-        text: `hello ${input?.name ?? 'world'}`,
+        text: `hello ${input?.name ?? "world"}`,
         // 💡 Tip: Try adding a new property here and see it propagate to the client straight-away
       };
     }),
@@ -35,9 +34,3 @@ const appRouter = t.router({
 // export only the type definition of the API
 // None of the actual implementation is exposed to the client
 export type AppRouter = typeof appRouter;
-
-// export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => ({}),
-});
